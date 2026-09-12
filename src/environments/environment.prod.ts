bb8d38773433c29c6d8924638f4f3831f4b9346b
ABDELGHANY30 <ABDELGHANY30@example.com>
2026-09-12 19:18:45 +0300

    Fix: add both local and prod environment files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://192.168.1.8:8000/api/v1', // بدون / في النهاية
+  // apiUrl: 'http://192.168.1.8:8000/api/v1', // بدون / في النهاية
+   apiUrl:'https://poultry-fastapi-backend.onrender.com'
+
 };
