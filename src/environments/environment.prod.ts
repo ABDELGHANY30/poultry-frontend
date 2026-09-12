bd5c43dc91dc7c5f5338d80bfc442affc60fd639
ABDELGHANY30 <ABDELGHANY30@example.com>
2026-09-12 19:24:15 +0300

    Fix: add both local and prod environment files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,4 @@
 export const environment = {
-  production: false,
-  // apiUrl: 'http://192.168.1.8:8000/api/v1', // بدون / في النهاية
-   apiUrl:'https://poultry-fastapi-backend.onrender.com'
-
+  production: true,
+  apiUrl: 'https://poultry-fastapi-backend.onrender.com/api' // أضف /api في النهاية
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,12 @@
 //   production: false,
 //   apiUrl: 'http://127.0.0.1:8000/api/v1',
 // };
+// export const environment = {
+//   production: false,
+//   // apiUrl: 'http://192.168.1.8:8000/api/v1', // بدون / في النهاية
+//    apiUrl:'https://poultry-fastapi-backend.onrender.com'
+// };
 export const environment = {
-  production: false,
-  // apiUrl: 'http://192.168.1.8:8000/api/v1', // بدون / في النهاية
-   apiUrl:'https://poultry-fastapi-backend.onrender.com'
+  production: true,
+  apiUrl: 'https://poultry-fastapi-backend.onrender.com/api' // أضف /api في النهاية
 };
