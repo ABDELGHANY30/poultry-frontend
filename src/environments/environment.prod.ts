769ac2c79af5dc315db79e16127b34a0c014c06f
ABDELGHANY30 <ABDELGHANY30@example.com>
2026-09-23 07:55:03 +0300

    update Fbackend

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://poultry-fastapi-backend.onrender.com/api/v1'
+  apiUrl: 'https://poultry-fastapi-backend.onrender.com/api/v1',
+  websiteUrl:"https://poultry-fastapi-backend.onrender.com/api/v1"
+
 };
