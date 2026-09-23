0cf5849477438c72e239add6f62aa7213f008735
ABDELGHANY30 <ABDELGHANY30@example.com>
2026-09-23 10:03:54 +0300

    update Fbackend

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://poultry-fastapi-backend.onrender.com/api/v1',
-  websiteUrl:"https://poultry-fastapi-backend.onrender.com/api/v1"
-
+  apiUrl: 'https://poultry-fastapi-backend.onrender.com/api/v1',  // الباك اند
+  websiteUrl: 'https://poultry-frontend-k52w.onrender.com',        // ⚠️ الفرونت إند (ده اللي كان غلط)
 };
